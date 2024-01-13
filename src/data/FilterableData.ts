87675c807fd1dc7c4223a4ee4ab878e24b3a67a6
jimenezj0r63 <jorgitojimlopez04@gmail.com>
2024-01-13 14:43:41 -0600

    primer commit actualizado

---
+++ src/data/FilterableData.ts
@@ -0,0 +1,99 @@
+export const filterableData = [
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/replicate-prediction-u6cwvubblf5iokb2chuomq7vbu(1).png",
+        title: "Sophia Deluxe",
+        text: "Sophia deluxe in big dick."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/replicate-prediction-stks3ojb6zc7lq56lx77ctvqhe(1).png",
+        title: "Porn",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "people",
+        src: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=800",
+        title: "Modeller",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "nature",
+        src: "https://res.cloudinary.com/dcamw2xs1/image/upload/v1705035961/Private/h3uuwo0eij4m2l45c9e6.png",
+        title: "Sea Life",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "cars",
+        src: "https://res.cloudinary.com/dcamw2xs1/image/upload/v1705035854/Private/fngguwzmhrukkbcwud09.png",
+        title: "Lamborghini",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "people",
+        src: "https://images.pexels.com/photos/432059/pexels-photo-432059.jpeg?auto=compress&cs=tinysrgb&w=800",
+        title: "Artist",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/replicate-prediction-stks3ojb6zc7lq56lx77ctvqhe(1).png",
+        title: "Forest",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/replicate-prediction-u6cwvubblf5iokb2chuomq7vbu(1).png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "https://res.cloudinary.com/dcamw2xs1/image/upload/v1705035673/Private/ci3tsizrwjvvbegjtb6i.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "https://res.cloudinary.com/dcamw2xs1/image/upload/v1705036439/Private/lgr6eaoye9mby8hbjcwr.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "https://photos.app.goo.gl/vH9e8Aqu7zMrWedu8",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/sophia.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/deluxe.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Sophia Deluxe",
+        src: "/src/data/images/kendra.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Porn",
+        src: "/src/data/images/replicate-prediction-stks3ojb6zc7lq56lx77ctvqhe.png",
+        title: "Sophia Deluxe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+   
+]
